ed1b14e00223a56616fdc79f54c74c5bd817e399
suryaa6666 <suryaelidanto@gmail.com>
2023-06-21 10:15:28 +0700

    feat : ui revision

--- Data Type/basic/script.js
+++ Data Type/basic/script.js
@@ -1,11 +1,8 @@
-let data = document.getElementById("name")
-let dataType = document.getElementById("type-name")
+let data = document.getElementById("name");
+let dataType = document.getElementById("type-name");
 
 // complete this code
-let yourData = 23
+let yourData = "Cintara SUrya Elidanto";
 
-data.textContent = `Halo nama saya ${yourData}`
-dataType.innerHTML = `<p>tipe data nama kamu itu <mark>${typeof (yourData)}</mark></p>`
-
-
-
+data.textContent = `Halo nama saya ${yourData}`;
+dataType.innerHTML = `<p>tipe data nama kamu itu <mark>${typeof yourData}</mark></p>`;

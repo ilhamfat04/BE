039ca9f438f5f9f441c66788b85238c44de6bce0
suryaa6666 <suryaelidanto@gmail.com>
2023-06-21 14:22:21 +0700

    feat : ui revision in looping

--- Data Type/complex/object.js
+++ Data Type/complex/object.js
@@ -12,7 +12,7 @@ for (let i = 0; i < 1; i++) {
         <div class="card col-sm-4">
             <div class="card-body">
                 <h5 class="card-title">${data.name}</h5>
-                <img src="${data.image}" class="card-img-top">
+                <img src="${data.image}" class="card-img">
                 <p class="card-text">Age : ${data.age}</p>
                 <a href="#" class="btn btn-primary">Like <i class="fa-regular fa-thumbs-up"></i></a>
             </div>
